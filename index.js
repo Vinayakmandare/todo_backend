b8109cd6e026862e6e5f848b2ae1b94664ae56c9
Vinayak Mandare <85034333+Vinayakmandare@users.noreply.github.com>
2024-11-24 19:43:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const TodoModel = require('./Models/Todo')
 const app = express()
 app.use(cors())
 app.use(express.json())
-
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log("MongoDB connection error:", err);
 });
+
 
 
 app.get('/get',(req,res) =>{
