60609c4d0df2b963c5dd3d286fe2b41069957a1a
Vinayak Mandare <85034333+Vinayakmandare@users.noreply.github.com>
2024-11-28 06:45:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const TodoModel = require('./Models/Todo')
 const app = express()
-const corsOptions = {
-  origin: '*',  // This allows all origins
-};
-app.use(cors(corsOptions));
-
-
+app.use(cors())
 app.use(express.json())
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((err) => {
-    console.log("MongoDB connection error:", err);
-});
 
 
+const uri = "mongodb+srv://vinayakmandare21:ZaolhK5sA3OqcY61@cluster0.8dewo.mongodb.net/todolist";
+console.log('MongoDB URI:', uri); // Debugging
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
-app.get('/get',(req,res) =>{
-    TodoModel.find()
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
-
+    app.get('/get', (req, res) => {
+        TodoModel.find()
+            .then(result => {
+                console.log('Fetched data:', result); // Debugging
+                res.json(result);
+            })
+            .catch(err => {
+                console.error('Error fetching data:', err); // Debugging
+                res.status(500).json(err);
+            });
+    });
+    
+    app.put('/edit/:id', async (req, res) => {
+        console.log('Request Params:', req.params);
+        console.log('Request Body:', req.body);
+    
+        const { id } = req.params;
+        const { task } = req.body;
+    
+        try {
+            const updatedTodo = await Todo.findByIdAndUpdate(id, { task }, { new: true });
+            if (!updatedTodo) {
+                console.error('Todo not found:', id);
+                return res.status(404).send('Todo not found');
+            }
+            console.log('Successfully updated:', updatedTodo);
+            res.json(updatedTodo);
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).send('Error updating task');
+        }
+    });
+    
+    
 app.put('/update/:id',(req,res)=>{
 const {id} = req.params;
 TodoModel.findByIdAndUpdate({_id:id},{done:true})
@@ -53,3 +73,4 @@ app.post('/add',(req, res)=>{
 app.listen(3001, ()=>{
     console.log("server is running")
 })
+
